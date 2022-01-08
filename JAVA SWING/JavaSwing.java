e0546424e77df589944d95d4cf7679b9ffdeff1b
Shreedhar73 <shreedharpandey000@gmail.com>
2022-01-08 13:40:21 +0545

    multiplied 2 numbers using swing

--- JAVA SWING/JavaSwing.java
+++ JAVA SWING/JavaSwing.java
@@ -1,5 +1,18 @@
-package com.bsc.java;
 
+import javax.swing.*;
 public class JavaSwing {
+    public static void main(String[] args) {
+        JFrame f = new JFrame();
+        JPanel p = new JPanel();
+        JButton b = new JButton("clickme");
+        b.setBounds(130,100,150,150);
+        p.add(b);
+        f.add(p);
+        p.setSize(800,400);
+        f.setSize(400,500);
+        f.setLayout(null);
+        f.setVisible(true);
+
+    }
 
 }
---
+++ JAVA SWING/multiply.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+ class Multiply extends JFrame implements ActionListener{
+     JLabel l1,l2;
+     JTextField t1,t2,t3;
+     JButton b1;
+
+     public Multiply(){
+
+        //UI
+         l1 = new JLabel("First Number");
+         l1.setBounds(0,0,0,0);
+        
+        t1 = new JTextField();
+        t1.setBounds(90,280,100,20);
+
+        l2 = new JLabel("Second Number:");
+        l2.setBounds(0,350,120,30);
+
+        t2 = new JTextField();
+        t2.setBounds(150, 350,150,30);
+
+        t3 = new JTextField();
+        t3.setBounds(0,400,150,30);
+
+        b1 = new JButton("Result");
+        b1.setBounds(10,450,150,40);
+        add(b1);
+        add(t3);
+        add(t2);
+        add(l2);
+        add(t1);
+        add(l1);
+
+        b1.addActionListener(this);
+
+        
+         
+         setSize(800,600);
+         setVisible(true);
+
+     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        if(e.getSource() == b1){
+            int num1 = Integer.parseInt(t1.getText());
+            int num2 = Integer.parseInt(t2.getText());
+            int product = num1 * num2;
+            t3.setText(String.valueOf(product));
+        }
+        
+    }
+    public static void main(String[] args) {
+        new Multiply();
+    }
+
+    
+}
